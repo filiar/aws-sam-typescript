5d574ea868664b384e95e32246100926dfd0784b
Iliya Chepishev <iliya_chepishev@epam.com>
2020-09-21 12:17:28 +0300

    Lambda build and exported successfuly

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import {
 	APIGatewayProxyEvent,
 	APIGatewayProxyHandler,
 	APIGatewayProxyResult,
+	Context,
 } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async (
 	event: APIGatewayProxyEvent,
+	context: Context
 ): Promise<APIGatewayProxyResult> => {
 	const queries = JSON.stringify(event.queryStringParameters);
 	return {
 		statusCode: 200,
-		body: `Queries: ${queries}`,
+		body: `Queries: ${ queries } ${ context.logStreamName }`,
 	};
 };
